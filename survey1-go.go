234d4b324a040ab431891c82433bd82a47374db1
Kyle Kuminkoski <krk0020@mix.wvu.edu>
2023-04-11 21:04:55 -0400

    Finished hypercake calculator

--- survey1-go.go
+++ survey1-go.go
@@ -1,11 +1,19 @@
+//Co-authors: Kyle Kuminkoski and Grant Stumpf
+
 package main
 
 import "fmt"
 
 func main() {
+	var n int
+	var k int
 	factorialMap := make(map[int]int)
-	fmt.Println(factorial(7, factorialMap))
-	fmt.Println(factorialMap)
+	fmt.Print("Enter a value for n: ")
+	fmt.Scan(&n)
+	fmt.Print("Enter a value for k: ")
+	fmt.Scan(&k)
+	fmt.Print("Hypercake value: ")
+	fmt.Println(hypercake(n, k, factorialMap))
 }
 
 func factorial(n int, factorialMap map[int]int) int {
@@ -20,4 +28,21 @@ func factorial(n int, factorialMap map[int]int) int {
 		return newFactorial
 	}
 
+}
+
+func combinations(n int, r int, factorialMap map[int]int) int {
+	if 0 <= r && r >= n {
+		return 0
+	} else {
+		return factorial(n, factorialMap) / (factorial(r, factorialMap) * factorial(n-r, factorialMap))
+	}
+}
+
+func hypercake(n int, k int, factorialMap map[int]int) int {
+	var value = 0
+	for k >= 0 {
+		value = value + combinations(n, k, factorialMap)
+		k = k - 1
+	}
+	return value
 }
